79e204a361e68ff5db26f6f3fc80bf8029769a06
Alex Poputa <poputa.alex@gmail.com>
2024-08-26 20:31:19 +0300

    started get request

--- src/partials/DataFetch.js
+++ src/partials/DataFetch.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 
 const KEY = '45639968-4ab0c3e34d3afa9a12b28af2f';
 const ENDPOINT = 'https://pixabay.com/api/';
-const options = {
-    x-api-key
-}
+
 
 export default class DataFetch {
   constructor({ selector }) {
@@ -15,7 +13,7 @@ export default class DataFetch {
   }
   async fetchData(params){
     try {
-        const {data} = await axios.get(`${ENDPOINT}?q=${params}`)
+        const {data} = await axios.get(`${ENDPOINT}?key=${KEY}&q=${params}&per_page=30`)
         return data
     } catch (error) {
         return console.error(error)
