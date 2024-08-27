83c7a5aacd47f4b70eabc7239436edb7746279f1
unknown <poputa.alex@gmail.com>
2024-08-27 16:18:56 +0300

    finished gallery and initial request

--- src/index.js
+++ src/index.js
@@ -1,9 +1,56 @@
 import { fetchData } from './partials/data-fetch';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
+const gallery = document.querySelector('.gallery');
+
+
+gallery.addEventListener('click', e => {
+  e.preventDefault();
+  if(e.target.nodeName != 'IMG'){
+    return
+  }
+  let imgGallery = new SimpleLightbox('.gallery a');
+  imgGallery.on('closed.simplelightbox', () => {
+    imgGallery.refresh();
+  });
+});
 
 document.querySelector('.search-form').addEventListener('submit', e => {
   e.preventDefault();
-  const {elements} = e.target
-  fetchData(elements[0].value)
+  const { elements } = e.target;
+  const options = {
+    params: elements[0].value,
+    perPage: 3,
+    page: 1,
+  };
+  fetchData(options).then(data => {
+    const { total, totalHits, hits } = data;
+    hits.forEach(({largeImageURL, previewURL,likes,views,comments,downloads}=obj)=>{
+      const card =`<a href="${largeImageURL}" class="gallery__link">
+                      <div class="card">
+                        <img class="card__img" src="${previewURL}" />
+                        <div class="card__text-box">
+                          <div class="card__descr-box">
+                            <h3 class="card__descr-title">Likes</h3>
+                            <p class="card__descr">${likes}</p>
+                            </div>
+                          <div class="card__descr-box">
+                            <h3 class="card__descr-title">Views</h3>
+                            <p class="card__descr">${views}</p>
+                          </div>
+                          <div class="card__descr-box">
+                            <h3 class="card__descr-title">Comments</h3>
+                            <p class="card__descr">${comments}</p>
+                          </div>
+                          <div class="card__descr-box">
+                            <h3 class="card__descr-title">Downloads</h3>
+                            <p class="card__descr">${downloads}</p>
+                          </div></div></div></a>`;
+      gallery.insertAdjacentHTML('beforeend', card)
+    })
+  });
 });
+
+
 
--- src/partials/data-fetch.js
+++ src/partials/data-fetch.js
@@ -4,10 +4,10 @@ const KEY = '45639968-4ab0c3e34d3afa9a12b28af2f';
 const ENDPOINT = 'https://pixabay.com/api/';
 
 
- export const fetchData = async (params)=>{
+ export const fetchData = async ({params,perPage,page})=>{
   try {
-    const {data} = await axios.get(`${ENDPOINT}?key=${KEY}&q=${params}&per_page=30`)
-    console.log(data)
+    const {data} = await axios.get(`${ENDPOINT}?key=${KEY}&q=${params}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${page}`)
+    return data
 } catch (error) {
     return console.error(error)
 }
