26e5308d51161a4719e49462b3613092805de675
Alex Poputa <poputa.alex@gmail.com>
2024-08-28 20:18:45 +0300

    finished homework

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { Notify } from 'notiflix';
-import { fetchData } from './partials/functions';
+import { fetchData, generateGallery } from './partials/functions';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
@@ -21,7 +21,7 @@ const resetPage = page => {
   page = 1;
 };
 
-
+//  getting initial data
 form.addEventListener('submit', e => {
   e.preventDefault();
   options.params = document.querySelector(
@@ -30,46 +30,14 @@ form.addEventListener('submit', e => {
   fetchData(options)
     .then(data => {
       resetPage(options.page);
-      const {total, totalHits, hits } = data;
+      const { totalHits, hits } = data;
       if (totalHits === 0) {
         throw new Error(
           'Sorry, there are no images matching your search query. Please try again.'
         );
       }
-      Notify.success(`Hooray! We found ${total} images.`)
-      hits.forEach(
-        ({
-          largeImageURL,
-          likes,
-          views,
-          comments,
-          downloads,
-          webformatURL,
-        } = obj) => {
-          const card = `<a href="${largeImageURL}" class="gallery__link">
-                      <div class="card">
-                        <img class="card__img" src="${webformatURL}" loading="lazy" />
-                        <div class="card__text-box">
-                          <div class="card__descr-box">
-                            <h3 class="card__descr-title">Likes</h3>
-                            <p class="card__descr">${likes}</p>
-                            </div>
-                          <div class="card__descr-box">
-                            <h3 class="card__descr-title">Views</h3>
-                            <p class="card__descr">${views}</p>
-                          </div>
-                          <div class="card__descr-box">
-                            <h3 class="card__descr-title">Comments</h3>
-                            <p class="card__descr">${comments}</p>
-                          </div>
-                          <div class="card__descr-box">
-                            <h3 class="card__descr-title">Downloads</h3>
-                            <p class="card__descr">${downloads}</p>
-                          </div></div></div></a>`;
-          gallery.insertAdjacentHTML('beforeend', card);
-          imgGallery.elements.push(card);
-        }
-      );
+      Notify.success(`Hooray! We found ${totalHits} images.`)
+      generateGallery(hits,gallery,imgGallery);
     })
     .catch(err => Notify.failure(err.message))
     .finally(() => {
@@ -78,18 +46,20 @@ form.addEventListener('submit', e => {
     });
 });
 
+
+// Infinite Loading
 window.addEventListener('scroll', e => {
-  scrollValue = Math.round(window.scrollY);
- if(scrollValue > 400){
+  scrollValue = Math.round(window.scrollY); // getting scroll position
+ if(scrollValue > 400){       //adding "top" button
   topBtn.classList.add('visible')
  } else{
   topBtn.classList.remove('visible')
  }
-  if (
+  if (                               // checink scroll position
     window.innerHeight + scrollValue >=
     document.body.offsetHeight
   ) {
-    fetchData(options)
+    fetchData(options)    // adding more data
       .then(data => {
         const { totalHits, hits } = data;
         if (totalHits === 0) {
@@ -97,39 +67,7 @@ window.addEventListener('scroll', e => {
             'Sorry, there are no images matching your search query. Please try again.'
           );
         }
-        hits.forEach(
-          ({
-            largeImageURL,
-            likes,
-            views,
-            comments,
-            downloads,
-            webformatURL,
-          } = obj) => {
-            const card = `<a href="${largeImageURL}" class="gallery__link">
-                      <div class="card">
-                        <img class="card__img" src="${webformatURL}" loading="lazy" />
-                        <div class="card__text-box">
-                          <div class="card__descr-box">
-                            <h3 class="card__descr-title">Likes</h3>
-                            <p class="card__descr">${likes}</p>
-                            </div>
-                          <div class="card__descr-box">
-                            <h3 class="card__descr-title">Views</h3>
-                            <p class="card__descr">${views}</p>
-                          </div>
-                          <div class="card__descr-box">
-                            <h3 class="card__descr-title">Comments</h3>
-                            <p class="card__descr">${comments}</p>
-                          </div>
-                          <div class="card__descr-box">
-                            <h3 class="card__descr-title">Downloads</h3>
-                            <p class="card__descr">${downloads}</p>
-                          </div></div></div></a>`;
-            gallery.insertAdjacentHTML('beforeend', card);
-            imgGallery.elements.push(card);
-          }
-        );
+        generateGallery(hits,gallery,imgGallery);
       })
       .catch(err => Notify.failure(err.message))
       .finally(() => {
@@ -139,6 +77,8 @@ window.addEventListener('scroll', e => {
   }
 });
 
+
+//  "to top" btn functionality
 topBtn.addEventListener('click',(e)=>{
   e.preventDefault();
   window.scrollTo({ top: 0, behavior: 'smooth' });
--- src/partials/functions.js
+++ src/partials/functions.js
@@ -4,18 +4,55 @@ const KEY = '45639968-4ab0c3e34d3afa9a12b28af2f';
 const ENDPOINT = 'https://pixabay.com/api/';
 
 const searchParams = new URLSearchParams({
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  });
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+});
 
-
- export const fetchData = async ({params,perPage,page})=>{
+// Grtting data
+export const fetchData = async ({ params, perPage, page }) => {
   try {
-    const {data} = await axios.get(`${ENDPOINT}?key=${KEY}&q=${params}&${searchParams}&per_page=${perPage}&page=${page}`)
-    return data
-} catch (error) {
-    return error
-}
-}
+    const { data } = await axios.get(
+      `${ENDPOINT}?key=${KEY}&q=${params}&${searchParams}&per_page=${perPage}&page=${page}`
+    );
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
 
+
+// function for populating the gallery
+export const generateGallery = (data, container, array) => {
+  if (data === undefined) {
+    throw new Error(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
+  data.forEach(
+    ({ largeImageURL, likes, views, comments, downloads, webformatURL }) => {
+      const card = `<a href="${largeImageURL}" class="gallery__link">
+                  <div class="card">
+                    <img class="card__img" src="${webformatURL}" loading="lazy" />
+                    <div class="card__text-box">
+                      <div class="card__descr-box">
+                        <h3 class="card__descr-title">Likes</h3>
+                        <p class="card__descr">${likes}</p>
+                        </div>
+                      <div class="card__descr-box">
+                        <h3 class="card__descr-title">Views</h3>
+                        <p class="card__descr">${views}</p>
+                      </div>
+                      <div class="card__descr-box">
+                        <h3 class="card__descr-title">Comments</h3>
+                        <p class="card__descr">${comments}</p>
+                      </div>
+                      <div class="card__descr-box">
+                        <h3 class="card__descr-title">Downloads</h3>
+                        <p class="card__descr">${downloads}</p>
+                      </div></div></div></a>`;
+      container.insertAdjacentHTML('beforeend', card);
+      array.elements.push(card);
+    }
+  );
+};
